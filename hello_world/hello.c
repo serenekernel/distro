813d99ba3387cc1365a5caf71ea4e26fee147697
watter-bottle <76277026+watter-bottle@users.noreply.github.com>
2026-03-27 17:00:29 +1100

    initramfs: stupid changes due to symlinks stuff and package bash

--- hello_world/hello.c
+++ hello_world/hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char **argv, char **envp) {
   printf("Hello, mlibc :3\n");
+  printf("argc=%d\n", argc);
+  for (int i = 0; i < argc; i++) {
+    printf("argv[%d]=%s\n", i, argv[i]);
+  }
+  for (int i = 0; envp[i] != NULL; i++) {
+    printf("envp[%d]=%s\n", i, envp[i]);
+  }
+
   return 0;
 }
